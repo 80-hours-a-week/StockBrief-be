e82e345bcc4f21687dc88e38c4bd78c2fc45c241
junwoo0725 <wnsgur4960@naver.com>
2026-06-18 14:25:10 +0900

    chore: point dev infra config to new aws account

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "stockbrief-terraform-state-420615923610-ap-northeast-2"
+    bucket         = "stockbrief-terraform-state-422110551937-ap-northeast-2"
     key            = "stockbrief/dev/terraform.tfstate"
     region         = "ap-northeast-2"
     dynamodb_table = "stockbrief-terraform-locks"
