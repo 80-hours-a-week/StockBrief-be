a5c1683611075b18dbe7fe414ac66aaf335baba3
junwoo0725 <wnsgur4960@naver.com>
2026-06-23 11:26:29 +0900

    운영 알람과 네트워크 egress 하드닝

--- infra/terraform/modules/rds_proxy/outputs.tf
+++ infra/terraform/modules/rds_proxy/outputs.tf
@@ -2,6 +2,10 @@ output "proxy_endpoint" {
   value = try(aws_db_proxy.postgres[0].endpoint, "")
 }
 
+output "proxy_name" {
+  value = try(aws_db_proxy.postgres[0].name, "")
+}
+
 output "proxy_arn" {
   value = try(aws_db_proxy.postgres[0].arn, "")
 }
